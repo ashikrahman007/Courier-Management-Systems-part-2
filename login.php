01da417224e45647cc8742f2b323dd74885ee693
ashikrahman007 <53301000+ashikrahman007@users.noreply.github.com>
2020-10-17 23:50:18 +0600

    Courier Management Systems part 2
    The updated version of Courier Management Systems (part 2)

---
+++ login.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Currier Service</title>
+		
+		
+		
+		
+
+		<meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		  <!-- nivo -->
+		  <link rel="stylesheet" href="css/nivo/default.css" type="text/css" media="screen" />
+		  <link rel="stylesheet" href="css/nivo/nivo-slider.css" type="text/css" media="screen" />
+		  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<!--<a class="navbar-brand" href="#"><img src="images/logo.jpeg" width="30%"></a>-->
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+				
+				<h1>Currier service Management System</h1>
+				<li class="active"><a href="login.php">Home</a></li>
+
+				<li class="active"><a href="login1.php">Login</a></li>
+				<li class="active"><a href="register.php">Register Now</a></li>
+				
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	
+	
+	
+	
+	
+	<div id="slider" class="nivoSlider">
+		<img src="images/img1.jpg" data-thumb="images/img1.jpg" alt="" />
+		<img src="images/img2.jpg" data-thumb="images/img2.jpg" alt=""/></a>
+		<img src="images/img3.jpg" data-thumb="images/img3.jpg" alt="" data-transition="slideInLeft" />
+		
+	</div>
+	
+		
+	</div>  -->
+	<!-- footer -->
+	<footer class="">
+		<div class="row">
+			<div class="col-md-4">
+				<h5><b>Location:</b></h5>
+				
+				<p><h6>H#54, Rd#11, Sector-10, Uttara</h6></P>
+				<p><h6>Dhaka, Bangladesh.</h6></p>
+				
+				<!-- <h5><b>Chittagong  Office:</b></h5>
+				<p><h6>149, Sugandha R/A, Panchlaish,</h6></p>
+				<p><h6>Chittagong-4203, Bangladesh.</h6></p>  -->
+			
+			</div>
+			
+			<div class="col-md-4">
+				<h5><b>Call Us:</b></h5>
+				<p><h6>Dial # 58815305 (Hunting)</h6></p>
+				<p><h6>+8801800000000</h6></p>
+				<p><h6>+8801600000000</h6></p>
+				<p><h6>Fax # 02-000000000</h6></p>
+				<p><h6>Hotline # 1230</h6></p>
+			</div>
+			<div class="col-md-4">
+				<h5><b>Email Us:</b></h5>
+				<p><h6>info@courrier1971.com</h6></p>
+				
+				
+			</div>
+			<div class="col-md-6 text-right">
+				<a href="https://www.facebook.com/" class="social fa fa-facebook"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.twitter.com/" class="social1 fa fa-twitter"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.google.com/" class="social2 fa fa-google"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.linkedin.com/" class="social3 fa fa-linkedin"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.youtube.com/" class="social4 fa fa-youtube"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+			</div>
+		
+		</div>
+	
+	</footer>
+	
+			
+	<script type="text/javascript" src="script/jquery.nivo.slider.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#slider').nivoSlider({
+			controlNav:false,
+			keyboardNav:false,
+			});
+		})
+	</script>
+	</body>
+</html>
---
+++ login1.php
@@ -0,0 +1,314 @@
+<?php  
+session_start(); 
+if(isset($_SESSION['admin_sid']) || isset($_SESSION['customer_sid']))
+{
+	header("location:index.php");
+}
+else{
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
+<body>
+
+
+
+<h2>                Restaurant Management System </h2>
+
+
+
+
+
+
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+</body>
+
+
+
+
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <title>Login</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#00bcd4">
+  <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+  
+  <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+    <!-- Custome CSS-->    
+    <link href="css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/layouts/page-center.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+  
+</head>
+
+<body class="cyan">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  
+  <head>
+<style>
+.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+.button1 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.button2 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #008CBA;
+}
+
+.button2:hover {
+    background-color: #008CBA;
+    color: white;
+}
+
+.button3 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #f44336;
+}
+
+.button3:hover {
+    background-color: #f44336;
+    color: white;
+}
+
+.button4 {
+    background-color: white;
+    color: black;
+    border: 2px solid #e7e7e7;
+}
+
+.button4:hover {background-color: #e7e7e7;}
+
+.button5 {
+    background-color: white;
+    color: black;
+    border: 2px solid #555555;
+}
+
+.button5:hover {
+    background-color: #555555;
+    color: white;
+}
+</style>
+</head>
+<body>
+
+
+
+
+
+
+<a href="login.php" class="button button3">Home</a>
+
+<a href="login1.php" class="button button3">Login</a>
+
+<a href="register.php" class="button button3">Register Now!</a>
+
+</body>
+
+
+  <div id="login-page" class="row">
+    <div class="col s12 z-depth-4 card-panel">
+      <form method="post" action="routers/router.php" class="login-form" id="form">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <p class="center login-form-text">Login for Food Ordering System</p>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+            <input name="username" id="username" type="text">
+            <label for="username" class="center-align">Username</label>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input name="password" id="password" type="password">
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">
+			<a href="javascript:void(0);" onclick="document.getElementById('form').submit();" class="btn waves-effect waves-light col s12">Login</a>
+          </div>
+		  		<div class="row">
+          <div class="input-field col s6 m6 l6">
+            <p class="margin medium-small"><a href="register.php">Register Now!</a></p>
+          </div>         
+        </div>
+        </div>
+
+
+      </form>
+    </div>
+  </div>
+
+
+
+  <!-- ================================================
+    Scripts
+    ================================================ -->
+
+  <!-- jQuery Library -->
+  <script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>
+  <!--materialize js-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <!--scrollbar-->
+  <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+      <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="js/plugins.min.js"></script>
+    <!--custom-script.js - Add your own theme custom JS-->
+    <script type="text/javascript" src="js/custom-script.js"></script>
+
+</body>
+</html>
+<?php
+}
+?>
